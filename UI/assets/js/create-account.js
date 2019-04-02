50935b82655014db278343f1b976b4173423a600
Tobechukwu <tobechukwuobitube@gmail.com>
2019-04-02 12:10:08 +0100

    [Feature #164983403] Add user(client) default dashboard profile

--- UI/assets/js/create-account.js
+++ UI/assets/js/create-account.js
@@ -3,7 +3,11 @@ const createAccount = document.getElementById('create-account');
 const createAccountForm = document.getElementById('create-account-form');
 const noAccount = document.getElementById('no-accounts');
 const create = document.getElementById('create');
+const accountProfile  = document.getElementById('account-profile');
 
+// createAccountForm.style.display = 'none';
+// accountCreated.style.display = 'none';
+// accountProfile.style.display = 'none';
 noAccount.style.display = 'block';
 
 const userCreateAccount = () => {
@@ -16,7 +20,9 @@ const createAccountBtn = (e) => {
   createAccountForm.style.display = 'none';
   e.preventDefault();
   noAccount.style.display = 'none';
+  accountProfile.style.display = 'block';
 };
+// accountProfile.style.display = 'block';
 
 createAccount.addEventListener('click', userCreateAccount);
 create.addEventListener('click', createAccountBtn);
