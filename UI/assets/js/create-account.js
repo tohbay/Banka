c2a471d67e9f574672be119180cfa002ac38f047
Tobechukwu <tobechukwuobitube@gmail.com>
2019-04-02 15:10:16 +0100

    [Feature #164983403] Add user view bank profile [Delivers #164983403]

--- UI/assets/js/create-account.js
+++ UI/assets/js/create-account.js
@@ -4,10 +4,10 @@ const createAccountForm = document.getElementById('create-account-form');
 const noAccount = document.getElementById('no-accounts');
 const create = document.getElementById('create');
 const accountProfile  = document.getElementById('account-profile');
+const viewProfile  = document.getElementById('view-profile');
 
-// createAccountForm.style.display = 'none';
-// accountCreated.style.display = 'none';
-// accountProfile.style.display = 'none';
+viewProfile.style.display = 'none';
+
 noAccount.style.display = 'block';
 
 const userCreateAccount = () => {
@@ -20,9 +20,18 @@ const createAccountBtn = (e) => {
   createAccountForm.style.display = 'none';
   e.preventDefault();
   noAccount.style.display = 'none';
-  accountProfile.style.display = 'block';
+  // accountProfile.style.display = 'block';
+  viewProfile.style.display = 'block';
 };
-// accountProfile.style.display = 'block';
+
+const showAccountProfile = () => {
+  accountProfile.style.display = 'block';
+  noAccount.style.display = 'none';
+  accountCreated.style.display = 'none';
+}
+
+viewProfile.disabled = false;
 
 createAccount.addEventListener('click', userCreateAccount);
 create.addEventListener('click', createAccountBtn);
+viewProfile.addEventListener('click', showAccountProfile);
