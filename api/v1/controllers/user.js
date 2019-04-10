8561b54b7a503e3e6c42f62b659a88c4ae67535e
Tobechukwu <tobechukwuobitube@gmail.com>
2019-04-10 12:56:07 +0100

    [Chore #165240028]: add signup and signin user controllers

--- api/v1/controllers/user.js
+++ api/v1/controllers/user.js
@@ -8,6 +8,24 @@ class userController {
       data: allUsers
     });
   }
+
+  static signup(request, response) {
+    const userdata = req.body;
+    const signupUser = UserService.add(userdata);
+    return response.status(201).send({
+      status: 201,
+      data: signupUser
+    });
+  }
+
+  static signin(request, response) {
+    const { email, password } = request.body;
+    const loginUser = UserService.add(request.body);
+    response.status(200).send({
+      status: 200,
+      data: loginUser
+    });
+  }
 }
 
 export default userController;
