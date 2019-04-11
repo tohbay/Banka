40cb35e2ade97b065a48bab4698490ea5f92ff13
Tobechukwu <tobechukwuobitube@gmail.com>
2019-04-12 00:49:32 +0100

    [Feature #165300207]: add mocha test for patch account

--- api/test/account.spec.js
+++ api/test/account.spec.js
@@ -9,10 +9,10 @@ const should = chai.should();
 
 describe('Mocha test for Account Controller', () => {
   describe('Mocha test for creating a bank account', () => {
-    const signupUrl = '/api/v1/accounts';
+    const createAccountUrl = '/api/v1/accounts';
     it('should register a new user when all the parameters are given', (done) => {
       chai.request(app)
-        .post(signupUrl)
+        .post(createAccountUrl)
         .send({
           type: 'savings' || 'current',
         })
@@ -22,9 +22,13 @@ describe('Mocha test for Account Controller', () => {
           expect(response.body.status).to.equal(201);
           expect(response.data).to.be.an('object');
           expect(response.body.data).to.have.property('id');
+          expect(response.body.data).to.have.property('accountNumber');
           expect(response.body.data).to.have.property('firstName');
           expect(response.body.data).to.have.property('lastName');
+          expect(response.body.data).to.have.property('createdOn');
+          expect(response.body.data).to.have.property('openingBalance');
           expect(response.body.data).to.have.property('email');
+          expect(response.body.data).to.have.property('type');
           expect(response.body.data.email).to.equal('frank@email.com');
           console.log(response.body);
           done();
@@ -33,7 +37,7 @@ describe('Mocha test for Account Controller', () => {
 
     it('should not create a bank account when the type is missing', (done) => {
       chai.request(app)
-        .post(signupUrl)
+        .post(createAccountUrl)
         .send({
 
         })
@@ -48,7 +52,7 @@ describe('Mocha test for Account Controller', () => {
 
     it('should not create an account when accountNumber already exist', (done) => {
       chai.request(app)
-        .post(signupUrl)
+        .post(createAccountUrl)
         .send({
           id: 1,
           accountNumber: 1,
@@ -56,8 +60,65 @@ describe('Mocha test for Account Controller', () => {
           owner: 1,
           type: 'savings',
           status: 'draft',
-          balance: 0.00
+          openingBalance: 0.00
         })
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.body.status).to.equal(400);
+          expect(response.body.error).to.be.a('string');
+          expect(response.body.error).to.equal('First name is required');
+          done();
+        });
+    });
+  });
+
+  describe('Mocha test for PATCH request on a bank account', () => {
+    const patchUrl = '/api/v1/accounts/:accountNumber';
+    it('should patch a selected account number when all the parameters are given', (done) => {
+      chai.request(app)
+        .post(patchUrl)
+        .send({
+          status: 'active' || 'dormant',
+        })
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.status).to.equal(201);
+          expect(response.body.status).to.equal(201);
+          expect(response.data).to.be.an('object');
+          expect(response.body.data).to.have.property('id');
+          expect(response.body.data).to.have.property('accountNumber');
+          expect(response.body.data).to.have.property('firstName');
+          expect(response.body.data).to.have.property('lastName');
+          expect(response.body.data).to.have.property('createdOn');
+          expect(response.body.data).to.have.property('openingBalance');
+          expect(response.body.data).to.have.property('email');
+          expect(response.body.data).to.have.property('type');
+          expect(response.body.data).to.have.property('status');
+          expect(response.body.data.email).to.equal('frank@email.com');
+          console.log(response.body);
+          done();
+        });
+    });
+
+    it('should not patch the account number when the status is missing', (done) => {
+      chai.request(app)
+        .post(createAccountUrl)
+        .send({
+
+        })
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.body.status).to.equal(400);
+          expect(response.body.error).to.be.a('string');
+          expect(response.body.error).to.equal('Email is required');
+          done();
+        });
+    });
+
+    it('should not patch the account number when accountNumber does not exist', (done) => {
+      chai.request(app)
+        .post(createAccountUrl)
+        .send({ })
         .end((error, response) => {
           expect(response.body).to.be.an('object');
           expect(response.body.status).to.equal(400);
